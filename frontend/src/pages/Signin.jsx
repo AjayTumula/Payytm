5c4fcf2fbf7cc8823ada5ed0f75f4aa823273e3f
AjayTumula <ajaytumula72@gmail.com>
2024-09-14 15:54:26 +0530

    add components for paytm-Ui

---
+++ frontend/src/pages/Signin.jsx
@@ -0,0 +1,11 @@
+import Heading from "../components/Heading";
+
+
+
+const Signin = () => {
+    return <div>
+        <Heading label={'Sign In'}/>
+    </div>
+}
+
+export default Signin;
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,28 @@
+import BottomWarning from "../components/BottomWarning";
+import Button from "../components/Button";
+import Heading from "../components/Heading";
+import InputBox from "../components/InputTextBox";
+import SubHeading from "../components/SubHeading";
+
+
+
+
+const Signup = () => {
+
+    return <div className="bg-slate-300 h-screen flex justify-center items-center">
+        <div className="bg-white rounded-lg w-[80%] sm:w-[50%] lg:w-[23%] text-center p-3">
+        <div className="flex flex-col">
+        <Heading label={'Signup Up'}/>
+        <SubHeading label={'Enter your information to create an account'}/>
+        <InputBox label={'First Name'} placeholder={'John'} />
+        <InputBox label={'Last Name'} placeholder={'Doe'} />
+        <InputBox label={'Email'} placeholder={'johndoe@example.com'} />
+        <InputBox label={'Password'} placeholder={''} />
+        <Button label={'Sign Up'}></Button>
+        <BottomWarning label={'Already have an account?'} buttonText={'Login'} to={'/signin'}/>
+    </div>
+    </div>
+    </div>
+}
+
+export default Signup;
